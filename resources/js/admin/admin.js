2249be24d4e8171280382ca7fc4d80989ff1ac23
Chu Xuan Thang <chu.xuan.thang@framgia.com>
2018-12-31 14:02:39 +0700

    show search User

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,7 +1,26 @@
 (function($) {
-    $('#sidebarToggle').on('click',function(e) {
+    'use strict';
+    $('#sidebarToggle').on('click', function(e) {
         e.preventDefault();
         $('body').toggleClass('sidebar-toggled');
         $('.sidebar').toggleClass('toggled');
+    });
+    $('#search').on('keyup', function() {
+        var $value = $(this).val();
+        $.ajax ({
+            type : 'get',
+            url : '/admin/user/search',
+            data : {
+                'search' : $value,
+            },
+            success : function(data) {
+                $('tbody').html(data);
+            }
+        });
+    });
+    $.ajaxSetup({
+        headers : {
+            'csrftoken' : '{{ csrf_token() }}' 
+        }
     });
 })(jQuery);
--- resources/lang/vi/admin.php
+++ resources/lang/vi/admin.php
@@ -2,6 +2,14 @@
 return [
     'logout' => 'Đăng xuất',
     'cancel' => 'Hủy',
+    'male' => 'Nam',
+    'female' => 'Nữ',
+    'admin' => 'Quản trị',
+    'active' => 'Hiện',
+    'deactive' => 'Ẩn',
+    'edit' => 'Sửa',
+    'delete' => 'Xóa',
+    'welcome' => 'Xin chào',
 
     'category' => [
         'user' => 'Người dùng',
